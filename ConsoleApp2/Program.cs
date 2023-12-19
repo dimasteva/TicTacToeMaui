dbb2ed58e0251ca2d7f7f9b5a9aa94197308b9c5
Dimitrije Stevanovic <dimitrije12012007@gmail.com>
2023-12-19 21:36:06 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Numerics;
+
+namespace ConsoleApp2
+{
+    class TicTacToe
+    {
+        public enum FieldStatus
+        {
+            Empty,
+            X,
+            O
+        }
+        public struct FieldCoordinate
+        {
+            public int x;
+            public int y;
+        }
+
+        public FieldStatus[][] field = new FieldStatus[3][];
+        public TicTacToe()
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                field[i] = new FieldStatus[3];
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    field[i][j] = FieldStatus.Empty;
+                }
+            }
+        }
+        public void PrintField()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < field.Length; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    Console.Write(field[i][j]);
+                    if (j != 2)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("---------------------");
+            }
+            Console.WriteLine();
+        }
+
+        public int? CheckField()
+        {
+            int? won = null;
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i][0] != FieldStatus.Empty && field[i][0] == field[i][1] && field[i][1] == field[i][2])
+                {
+                    won = field[i][0] == FieldStatus.X ? 1 : 2;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[0][i] != FieldStatus.Empty && field[0][i] == field[1][i] && field[1][i] == field[2][i])
+                {
+                    won = field[0][i] == FieldStatus.X ? 1 : 2;
+                }
+            }
+
+            if (field[0][0] != FieldStatus.Empty && field[0][0] == field[1][1] && field[1][1] == field[2][2])
+            {
+                won = field[0][0] == FieldStatus.X ? 1 : 2;
+            }
+            if (field[0][2] != FieldStatus.Empty && field[0][2] == field[1][1] && field[1][1] == field[2][0])
+            {
+                won = field[0][2] == FieldStatus.X ? 1 : 2;
+            }
+
+            return won;
+        }
+
+        public int getColumnandRow(bool done)
+        {
+            int x = -1;
+            while (x < 1 || x > 3)
+            {
+                Console.WriteLine("Enter " + (done? "row" : "column") + "(1-3):");
+                try
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occured: " + ex.Message);
+                }
+                if (x < 1 || x > 3)
+                {
+                    Console.WriteLine((done ? "Row" : "Column ") + "is out of bounds");
+                }
+            }
+            return x;
+        }
+
+        private void PlaceMark(int row, int column, bool move)
+        {
+            field[row][column] = move ? FieldStatus.X : FieldStatus.O;
+        }
+
+        public void GameFlow()
+        {
+            PrintField();
+            int duration = 1;
+            bool move = true; //X plays
+            bool done = true; //For input
+            while(duration <= 9)
+            {
+                FieldCoordinate coordinates = new FieldCoordinate();
+                Console.WriteLine("The " + (move ? "X" : "O") + " player's move");
+                while (true)
+                {
+                    coordinates.x = getColumnandRow(done);
+                    done = !done;
+                    coordinates.y = getColumnandRow(done);
+                    done = !done;
+                    if (field[coordinates.x - 1][coordinates.y - 1] == FieldStatus.Empty)
+                        break;
+                }
+                PlaceMark(coordinates.x - 1, coordinates.y - 1, move);
+                int? winner = CheckField();
+                if (winner == 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The X player won");
+                    Console.WriteLine();
+                    PrintField();
+                    return;
+                }
+                else if (winner == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The O player won");
+                    PrintField();
+                    return;
+                }
+                move = !move;
+                duration++;
+                PrintField();
+            }
+            PrintField();
+            Console.WriteLine("Draw");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe Game = new TicTacToe();
+            Game.GameFlow();
+        }
+    }
+}
---
+++ TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,144 @@
+﻿
+namespace TicTacToe
+{
+    public partial class MainPage : ContentPage
+    {
+        bool move = true; //X = true, O = false
+        char[][] board = {
+            new char[] { ' ', ' ', ' ' },
+            new char[] { ' ', ' ', ' ' },
+            new char[] { ' ', ' ', ' ' }
+        };
+        int counter = 0;
+        int xScore = 0;
+        int oScore = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Button button = this.FindByName<Button>("button" + i);
+
+                if (button != null)
+                {
+                    button.Clicked += FieldClicked;
+                }
+            }
+        }
+        private void FieldClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.Text = move ? 'X'.ToString() : 'O'.ToString();
+            move = !move;
+            button.IsEnabled = false;
+
+            string s = button.ClassId;
+
+            char name = s[s.Length - 1];
+            int nameInt = name - '0';
+
+
+            int row = 0;
+
+            if (nameInt <= 3)
+                row = 0;
+            else if (nameInt >= 7)
+                row = 2;
+            else
+                row = 1;
+
+            int column = 0;
+            if (nameInt % 3 == 0)
+                column = 2;
+            else if (nameInt % 3 == 2)
+                column = 1;
+            else
+                column = 0;
+
+            board[row][column] = button.Text[0];
+
+            char c = CheckForWinner();
+            if (c != ' ')
+            {
+                DisplayAlert("Game over", c + " player won", "Ok");
+
+                ClearButtons();
+                ClearBoard();
+
+                move = true;
+                counter = 0;
+
+                if (c == 'X')
+                {
+                    xScore++;
+                    xPoints.Text = xScore.ToString();
+                }
+                else
+                {
+                    oScore++;
+                    oPoints.Text = oScore.ToString();
+                }
+            }
+            else
+                counter++;
+            if (counter == 9)
+            {
+                DisplayAlert("Game over", "It's a draw", "Ok");
+
+                ClearButtons();
+                ClearBoard();
+
+                move = true;
+                counter = 0;
+            }
+        }
+        private char CheckForWinner()
+        {
+            // Check horizontal lines
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+                    return board[i][0];
+            }
+
+            // Check vertical lines
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+                    return board[0][i];
+            }
+
+            // Check diagonals
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+                return board[0][0];
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
+                return board[0][2];
+
+            // No winner
+            return ' ';
+        }
+
+        private void ClearButtons()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                Button button = this.FindByName<Button>("button" + i);
+
+                if (button != null)
+                {
+                    button.Text = ' '.ToString();
+                    button.IsEnabled = true;
+                }
+            }
+        }
+
+        private void ClearBoard()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    board[i][j] = ' ';
+        }
+
+    }
+}
